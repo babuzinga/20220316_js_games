7a6609e191c22b442beecad1200917e0c3ac55fc
babuzinga <babuzinga@gmail.com>
2022-04-06 22:53:31 +1000

    2022-04-06 22-53

--- 1.php
+++ 1.php
@@ -21,46 +21,71 @@ echo '</table>';
 
 
 
-// , 'left', 'down'
-
-$m = [
-  ['x' => 1,  'y' => 0,   'x_n' => false, 'y_n' => 1],
+/*
+ * 1 - параметр - перебор по оси X
+ * 2 - параметр - перебор по оси Y
+ * 3 - параметр - смещение по оси X
+ * 4 - параметр - смещение по оси Y
+ */
+$methods = [
+  // RIGHT
+  [
+    'x'   => 1,
+    'y'   => 0,
+    'x_n' => 0,
+    'y_n' => 1,
+  ],
+  // DOWN
+  [
+    'x'   => 0,
+    'y'   => 1,
+    'x_n' => 1,
+    'y_n' => 0,
+  ],
+  // LEFT
+  [
+    'x'   => -1,
+    'y'   => 0,
+    'x_n' => 0,
+    'y_n' => 1,
+  ],
+  // UP
+  [
+    'x'   => 0,
+    'y'   => -1,
+    'x_n' => 1,
+    'y_n' => 0,
+  ],
 ];
 $graph = [];
-foreach (['right', 'top'] as $direction)
-  $graph = scanner($direction, 1, 1, false, false, $graph);
+foreach ($methods as $method)
+  $graph = scanner($method, 1, 1, false, false, $graph);
 
 print_array($graph);
 
-function scanner($direction, $base_x, $base_y, $scan_x, $scan_y, $arr) {
-  switch ($direction) {
-    case 'right':
-      $scan_x = $scan_x === false ? $base_x : $scan_x;
-      $scan_y = $scan_y === false ? 0       : $scan_y;
-      $node = getNode($scan_x, $scan_y);
 
-      if ($node_next = getNode($scan_x + 1, $scan_y)) {
-        $arr[$node][] = $node_next;
-        $arr = scanner($direction, 1, 1, $scan_x + 1, $scan_y, $arr);
-      } elseif (getNode($scan_x, $scan_y + 1)) {
-        $arr = scanner($direction, 1, 1, false, $scan_y + 1, $arr);
-      }
+/**
+ * @param $method
+ * @param $base_x
+ * @param $base_y
+ * @param $scan_x
+ * @param $scan_y
+ * @param $arr
+ * @return mixed
+ */
+function scanner($method, $base_x, $base_y, $scan_x, $scan_y, $arr) {
+  $scan_x = $scan_x === false ? ($method['x_n'] === 0 ? $base_x : 0) : $scan_x;
+  $scan_y = $scan_y === false ? ($method['y_n'] === 0 ? $base_y : 0) : $scan_y;
 
-      break;
+  $node = getNode($scan_x, $scan_y);
 
-    case 'top':
-      $scan_x = $scan_x === false ? 0       : $scan_x;
-      $scan_y = $scan_y === false ? $base_y : $scan_y;
-      $node = getNode($scan_x, $scan_y);
-
-      if ($node_next = getNode($scan_x, $scan_y + 1)) {
-        $arr[$node][] = $node_next;
-        $arr = scanner($direction, 1, 1, $scan_x, $scan_y + 1, $arr);
-      } elseif (getNode($scan_x + 1, $scan_y)) {
-        $arr = scanner($direction, 1, 1, $scan_x + 1, false, $arr);
-      }
-
-      break;
+  if ($node_next = getNode($scan_x + $method['x'], $scan_y + $method['y'])) {
+    $arr[$node][] = $node_next;
+    $arr = scanner($method, 1, 1, $scan_x + $method['x'], $scan_y + $method['y'], $arr);
+  } elseif (getNode($scan_x + $method['x_n'], $scan_y + $method['y_n'])) {
+    $scan_x_go = $method['x_n'] === 0 ? false : $scan_x + $method['x_n'];
+    $scan_y_go = $method['y_n'] === 0 ? false : $scan_y + $method['y_n'];
+    $arr = scanner($method, 1, 1, $scan_x_go, $scan_y_go, $arr);
   }
 
   if (!empty($node) && empty($arr[$node])) $arr[$node][] = null;
@@ -68,6 +93,11 @@ function scanner($direction, $base_x, $base_y, $scan_x, $scan_y, $arr) {
   return $arr;
 }
 
+/**
+ * @param $x
+ * @param $y
+ * @return bool|int|mixed
+ */
 function getNode($x, $y) {
   global $map;
   return !empty($map[$y][$x]) ? $map[$y][$x] : false;
